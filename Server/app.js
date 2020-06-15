452aca64652fbf9c8aa66b4527f92c717454b77d
Ekaterina Zhdanova <katherine.inspiration@gmail.com>
2020-06-15 22:32:53 +0300

    Fixed server settings. Now it's hosting on heroku without errors

--- Server/app.js
+++ Server/app.js
@@ -4,15 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+
+const port = process.env.PORT || 3000;
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,5 +36,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
 
 module.exports = app;
