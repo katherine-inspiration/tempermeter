8dc2f2f9666312720b54bc5928d51a5629f1e0dc
Ekaterina Zhdanova <katherine.inspiration@gmail.com>
2020-06-17 22:58:54 +0300

    Added Client code (again)

---
+++ Client/tempermeter/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+        Welcome to the TemperMeter!
+    </div>
+  );
+}
+
+export default App;
